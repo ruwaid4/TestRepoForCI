82a174a47bef0d188f413f2e9f9e134f198d14b5
ruwaid4 <ruwaid4@hotmail.com>
2020-02-05 15:51:45 +0100

    Update App.java

--- src/main/java/DD2480_group6/App.java
+++ src/main/java/DD2480_group6/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println(asdasd "Hello World!" );
+        System.out.println(asasddasd "Hello World!" );
     }
 }
